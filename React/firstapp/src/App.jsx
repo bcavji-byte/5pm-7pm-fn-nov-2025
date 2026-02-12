7a4cc5dd52d3e4bc322a78e846b0dfe381962de2
vijaykumar <bcavji@gmail.com>
2026-02-12 18:14:16 +0530

    mm

--- React/firstapp/src/App.jsx
+++ React/firstapp/src/App.jsx
@@ -38,7 +38,7 @@ function App() {
       </vijay>
         
       <button>
-        children = {click me}
+        children = {onClick}
       </button>
   
 
---
+++ React/firstapp/src/Todoapp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Todoapp() {
+  return (
+    <input type="text" placeholder='plase enter tasls'/>
+                   
+
+
+  )
+}
+
+export default Todoapp
